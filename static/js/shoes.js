b2cc1b1f3df9cf07ee8765c19df5bdc3ced90942
SeoulChonnom <bk688035@gmail.com>
2024-10-11 01:22:05 +0900

    feat: css 수정 및 shoes.js 추가

---
+++ static/js/shoes.js
@@ -0,0 +1,21 @@
+const shoesLink = {
+  nb574Div: "nb574",
+  nb530Div: "nb530",
+  nikeP6000Div: "p6000",
+  nikeV2KDiv: "v2k",
+  nikeZoomBomero5Div: "zoombomero5",
+  asicsJog100Div: "jog100",
+};
+
+function shoesClick(divId) {
+  console.log(shoesLink[divId]);
+}
+// 모든 해당 클래스의 div 요소를 선택
+const divs = document.querySelectorAll(".shoesDiv");
+
+// 각 div에 이벤트 리스너를 추가
+divs.forEach((div) => {
+  div.addEventListener("click", function () {
+    shoesClick(div.id); // 클릭된 div의 ID를 함수에 인자로 전달
+  });
+});
