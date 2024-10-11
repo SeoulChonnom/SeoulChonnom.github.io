5063368fe547f6d3354a64e6df70ba91ee6dad32
SeoulChonnom <bk688035@gmail.com>
2024-10-12 03:57:19 +0900

    feat: shoes.html 이름 변경

--- static/js/shoesRecom.js
+++ static/js/shoesRecom.js
@@ -8,13 +8,17 @@ const shoesLink = {
 };
 
 function shoesClick(divId) {
-  console.log(shoesLink[divId]);
+  //console.log("./shoes/" + shoesLink[divId] + ".html");
+  location.href = "./shoes/" + shoesLink[divId] + ".html";
 }
+
+document.addEventListener("DOMContentLoaded", function () {});
 // 모든 해당 클래스의 div 요소를 선택
 const divs = document.querySelectorAll(".shoesDiv");
 
 // 각 div에 이벤트 리스너를 추가
 divs.forEach((div) => {
+  console.log(div.id);
   div.addEventListener("click", function () {
     shoesClick(div.id); // 클릭된 div의 ID를 함수에 인자로 전달
   });
