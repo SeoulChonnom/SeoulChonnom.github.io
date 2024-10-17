228d3ee7a3123e13d907017af218903e770c501d
SeoulChonnom <bk688035@gmail.com>
2024-10-18 00:30:00 +0900

    feat: 메인페이지 CSS 임포트

--- src/App.vue
+++ src/App.vue
@@ -4,6 +4,7 @@
       <img id="SLCN" src="./assets/img/SLCN.png" />
     </div>
     <HelloWorld msg="Welcome to Your Vue.js App" />
+    <div id="footer">© Seoul CHONNOM</div>
   </main>
 </template>
 
@@ -19,35 +20,8 @@ export default {
 </script>
 
 <style>
+@import "./assets/css/common.css";
 #app {
-  /* font-family: Avenir, Helvetica, Arial, sans-serif; */
-  /* -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale; */
-  text-align: center;
-  /* color: #2c3e50; */
   background-color: rgb(241, 214, 208);
-}
-main {
-  background-color: rgb(252, 242, 242);
-  padding-top: 1vh;
-  padding-bottom: 1vh;
-  margin-left: auto;
-  margin-right: auto;
-  border-radius: 10px;
-  text-align: center;
-  /* height: 96vh; */
-  width: 50vh;
-}
-#header {
-  height: 10vh;
-  margin-left: 1rem;
-  margin-right: 1rem;
-  margin-bottom: 0.5vh;
-  border-radius: 10px;
-  background-color: rgb(252, 242, 242);
-}
-
-#SLCN {
-  height: 100%;
 }
 </style>
