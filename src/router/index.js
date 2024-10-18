6f9c5f469aa35cebf4d6c21e13f5f16a8bd12166
SeoulChonnom <bk688035@gmail.com>
2024-10-19 03:31:19 +0900

    feat: 상세 페이지 추가

--- src/router/index.js
+++ src/router/index.js
@@ -1,12 +1,18 @@
 import { createWebHistory, createRouter } from "vue-router";
 
 import mainPage from "@/views/mainPage.vue";
+import tripPage from "@/views/tripPage.vue";
 
 const routes = [
   {
     path: "/",
     name: "mainPage",
     component: mainPage,
+  },
+  {
+    path: "/map/:date",
+    name: "tripPage",
+    component: tripPage,
   },
 ]; //라우팅 패스, 컴포넌트 등 정의
 
---
+++ src/views/tripPage.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="content">
+    <div id="infoDiv">서울 촌놈 나들이 경로 😎</div>
+    <div id="mapDiv">
+      <img id="map" src="@/assets/img/map_20240929.png" />
+    </div>
+    <div id="photoDiv">
+      <a id="myboxLink" href="http://naver.me/GTnQ1tj1" target="_blank">
+        <img id="mybox" src="@/assets/img/mybox.png" />
+      </a>
+      <div id="myboxDiv">
+        사진은 드라이브에서 📷
+        <div id="pwDiv">암호 🔒 : 입사일</div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "tripPage",
+};
+</script>
+
+<style scoped>
+@import "@/assets/css/map.css";
+</style>
