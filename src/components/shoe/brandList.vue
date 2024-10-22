90eaf0f4d67792465267a32ccfb06ed8ba916f8f
SeoulChonnom <bk688035@gmail.com>
2024-10-22 23:58:41 +0900

    feat: brandList.vue 생성

---
+++ src/components/shoe/brandList.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="brandDiv" v-bind:id="brand.divId">
+    <img class="brandImg" :src="this.brand.img" />
+    <div class="brandInfo">
+      <div class="brandName">{{ brand.name }}</div>
+      <div class="brandDesc">{{ brand.desc }}</div>
+    </div>
+  </div>
+</template>
+  
+<script>
+export default {
+  name: "brandList",
+  props: { brand: Object },
+  methods: {},
+  data() {
+    return {};
+  },
+};
+</script>
+  
+<style>
+@import "@/assets/css/shoesRecom.css";
+</style>
