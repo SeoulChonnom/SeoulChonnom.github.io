2043c8453059e662a15a0210f12efb19661b8c05
SeoulChonnom <bk688035@gmail.com>
2024-10-22 23:36:04 +0900

    feat: 라우터 추가

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import { createWebHistory, createRouter } from "vue-router";
 
 import mainPage from "@/views/mainPage.vue";
 import tripPage from "@/views/tripPage.vue";
+import shoesRecom from "@/views/shoesRecom.vue";
 
 const routes = [
   {
@@ -14,9 +15,17 @@ const routes = [
     name: "tripPage",
     component: tripPage,
   },
+  {
+    path: "/shoesRecom",
+    name: "shoesRecom",
+    component: shoesRecom,
+  },
 ]; //라우팅 패스, 컴포넌트 등 정의
 
 export const router = createRouter({
+  scrollBehavior() {
+    return { top: 0 };
+  },
   history: createWebHistory(),
   routes: routes,
 });
