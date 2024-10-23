98d49ada96b5502a3fe4a62e923f976e859707e1
SeoulChonnom <bk688035@gmail.com>
2024-10-23 20:46:37 +0900

    feat: 신발 데이터 추가

--- src/global/global.js
+++ src/global/global.js
@@ -42,9 +42,10 @@ export const globalTrip = {
 };
 
 export const globalShoes = {
-  brands: {
-    nb: {
+  brands: [
+    {
       id: 1,
+      brandId: "nb",
       divId: "nbBrandDiv",
       name: "뉴발란스",
       desc: "발 편한건 역시 뉴발란스지~",
@@ -53,21 +54,26 @@ export const globalShoes = {
         {
           id: 1,
           divId: "nb574Div",
+          shoesId: "nb574",
           name: "뉴발란스 574",
+          desc: "동글동글 괜찮잖아. 귀엽잖아..?",
           price: "149,000원",
           img: require("@/assets/img/shoes/NB574.png"),
         },
         {
           id: 2,
           divId: "nb530Div",
+          shoesId: "nb530",
           name: "뉴발란스 530",
+          desc: "어디에 신어도 잘 어울리는 신발",
           price: "119,000원",
           img: require("@/assets/img/shoes/NB530.png"),
         },
       ],
     },
-    nike: {
+    {
       id: 2,
+      brandId: "nike",
       divId: "nikeBrandDiv",
       name: "나이키",
       desc: "나이키가 한물 갔다해도 나이키지~",
@@ -76,28 +82,35 @@ export const globalShoes = {
         {
           id: 1,
           divId: "nikeP6000Div",
+          shoesId: "p6000",
           name: "P-6000",
+          desc: "캐주얼 룩에 찰떡인 신발",
           price: "129,000원",
           img: require("@/assets/img/shoes/NikeP6000.png"),
         },
         {
           id: 2,
           divId: "nikeV2KDiv",
+          shoesId: "v2k",
           name: "V2K 런",
+          desc: "굉장히 인기가 많은 V2K",
           price: "139,000원",
           img: require("@/assets/img/shoes/NikeV2K.png"),
         },
         {
           id: 3,
           divId: "nikeZoomBomero5Div",
+          shoesId: "zoombomero5",
           name: "줌 보메로5",
+          desc: "뉴진스 민지의 추천 픽!",
           price: "189,000원",
           img: require("@/assets/img/shoes/NikeZoomBomero5.png"),
         },
       ],
     },
-    asics: {
+    {
       id: 3,
+      brandId: "asics",
       divId: "asicsBrandDiv",
       name: "아식스",
       desc: "요즘 MZ들 사이에서 핫한 아식스!",
@@ -106,11 +119,13 @@ export const globalShoes = {
         {
           id: 1,
           divId: "asicsJog100Div",
+          shoesId: "jog100",
           name: "조그 100",
+          desc: "가성비하면 이 신발이지!",
           price: "79,000원",
           img: require("@/assets/img/shoes/AsicsJog100.png"),
         },
       ],
     },
-  },
+  ],
 };
