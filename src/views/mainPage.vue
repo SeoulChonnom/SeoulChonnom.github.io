2eaa1065b4dd8d93121234d0a98e792a9ac3bb59
SeoulChonnom <bk688035@gmail.com>
2024-11-16 02:04:08 +0900

    feat: 신발추천 이동

--- src/views/mainPage.vue
+++ src/views/mainPage.vue
@@ -5,8 +5,7 @@
       <tripList v-for="trip in trips" :key="trip.date" v-bind:trip="trip" />
     </div>
     <div id="shoesRecomButtonDiv" @click="onclickShoes">
-      월 평균 10000보의 주인공의 신발 추천이 궁금하다면???<br />
-      여길 클릭해봐 😆
+      서울 촌놈의 신발 추천~👟
     </div>
   </div>
 </template>
